e5ae2070a8e738974042bc8c10d20245adc64bd5
madhavan-raja <madhavanraja99@gmail.com>
2022-07-11 16:19:54 +0530

    Info is being saved now

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,44 @@
-use furdb::{FurColumn, FurDB, FurDBInfo, FurDataType, FurTable, FurTableInfo};
-use std::error::Error;
+use furdb::{FurColumn, FurDB, FurDBInfo, FurDataType, FurTableInfo};
+use std::{collections::HashMap, error::Error, path::PathBuf};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let db_path = PathBuf::from("D:\\Home\\Repositories\\FurDB\\TestDBs\\PersonData");
+    let db_path = PathBuf::from("D:\\Home\\Repositories\\FurDB\\TestDBs\\Dictionary");
     let db_info = FurDBInfo::new("Dictionary")?;
 
-    let db = FurDB::new(db_path, Some(db_info));
+    let db = FurDB::new(db_path, Some(db_info))?;
 
-    let table_info = FurTableInfo::new();
+    let table_info = FurTableInfo::new(
+        "Dictionary",
+        Some("http://localhost:5000"),
+        Some(vec![
+            FurColumn::new(
+                "word",
+                Some("Word"),
+                80,
+                FurDataType::new("long_string", None)?,
+            )?,
+            FurColumn::new(
+                "definition",
+                Some("Definition"),
+                240,
+                FurDataType::new("long_string", None)?,
+            )?,
+        ]),
+    )?;
+
+    let mut table = db.get_table("dictionary", Some(table_info))?;
+    table.save_info()?;
+
+    let words = [
+        HashMap::from([("word", "7"), ("definition", "18")]),
+        HashMap::from([("word", "7"), ("definition", "18")]),
+        HashMap::from([("word", "7"), ("definition", "18")]),
+        HashMap::from([("word", "7"), ("definition", "18")]),
+        HashMap::from([("word", "7"), ("definition", "18")]),
+    ];
+
+    table.add(&words).await?;
 
     Ok(())
 }
